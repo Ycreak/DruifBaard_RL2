cf5a87b92ea094ff6bc3045c04c450c3b55dd3b6
ppbors <ppbors@hotmail.com>
2021-03-02 18:38:11 +0100

    Useful comments

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+# On Mac:
+#   pip3 install gym
+#   brew install glfw3
+#   brew install glew
+
+# observation --> 4-tuple: [position of cart, velocity of cart, angle of pole, rotation rate of pole]
+# reward --> always 1: for every step the pole does not fall
+# done --> boolean: becomes true if the pole is certainly going to fall
+# info --> for debugging
+
 import gym
 env = gym.make('CartPole-v0')
 for i_episode in range(20):
