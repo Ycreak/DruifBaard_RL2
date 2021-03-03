c3c70f06cdbdd7425e3af42488c7c5142937176c
ppbors <ppbors@hotmail.com>
2021-03-03 11:50:50 +0100

    Bugfix

--- main.py
+++ main.py
@@ -18,15 +18,22 @@ LIM_CART_VELOCITY = 1.0
 LIM_POLE_ANGLE = 12.0
 LIM_POLE_ROTATION = 25
 
-STEP_CART_POSITION = 0.1
+STEP_CART_POSITION = 0.01
 STEP_CART_VELOCITY = 0.01
 STEP_POLE_ANGLE = 0.1
-STEP_POLE_ROTATION = 0.01
+STEP_POLE_ROTATION = 0.1
 
 NUMSTEPS_CART_POSITION = int((LIM_CART_POSITION * 2) / STEP_CART_POSITION)
 NUMSTEPS_CART_VELOCITY = int((LIM_CART_VELOCITY * 2) / STEP_CART_VELOCITY)
 NUMSTEPS_POLE_ANGLE = int((LIM_POLE_ANGLE * 2) / STEP_POLE_ANGLE)
 NUMSTEPS_POLE_ROTATION = int((LIM_POLE_ROTATION * 2) / STEP_POLE_ROTATION)
+
+print(NUMSTEPS_CART_POSITION)
+print(NUMSTEPS_CART_VELOCITY)
+print(NUMSTEPS_POLE_ANGLE)
+print(NUMSTEPS_POLE_ROTATION)
+
+
 
 STATE_SIZE = NUMSTEPS_CART_POSITION * NUMSTEPS_CART_VELOCITY * NUMSTEPS_POLE_ANGLE * NUMSTEPS_POLE_ROTATION
 ACTION_SIZE = 2
@@ -40,32 +47,32 @@ def discretize(observation) -> int: # Maybe reject values outside selected space
     cart_position, cart_velocity, pole_angle, pole_rotation = observation
     
     # calculate orders first
-    cart_position = int((round(cart_position, 1) + LIM_CART_POSITION) / STEP_CART_POSITION)
+    cart_position = int((round(cart_position, 2) + LIM_CART_POSITION) / STEP_CART_POSITION)
     cart_velocity = int((round(cart_velocity, 2) + LIM_CART_VELOCITY) / STEP_CART_VELOCITY)
     pole_angle = int((round(pole_angle, 1) + LIM_POLE_ANGLE) / STEP_POLE_ANGLE)
     pole_rotation = int((round(pole_rotation, 1) + LIM_POLE_ROTATION) / STEP_POLE_ROTATION)
 
     index = cart_position
     index = index + NUMSTEPS_CART_POSITION * cart_velocity
-    index = index + NUMSTEPS_CART_VELOCITY * pole_angle
-    index = index + NUMSTEPS_POLE_ANGLE * pole_rotation
+    index = index + NUMSTEPS_CART_POSITION * NUMSTEPS_CART_VELOCITY * pole_angle
+    index = index + NUMSTEPS_CART_POSITION * NUMSTEPS_CART_VELOCITY * NUMSTEPS_POLE_ANGLE * pole_rotation
     return index
 
 # The matrix
-Q = np.zeros((STATE_SIZE, ACTION_SIZE))
+Q = np.zeros([STATE_SIZE, ACTION_SIZE])
 
 env = gym.make('CartPole-v0')
 env.reset()
 
 total_timesteps = 0
-for episode in range(100000):
+for episode in range(1000000):
     done = False
     total_reward = 0
     state = env.reset()
+    state_index = discretize(state)
     t = 0
     while not done:
         #env.render()
-        state_index = discretize(state)
         if random.uniform(0, 1) < epsilon:
             action = env.action_space.sample() # Explore state space
         else:
@@ -73,15 +80,19 @@ for episode in range(100000):
 
         next_state, reward, done, info = env.step(action) # invoke Gym
         next_state_index = discretize(next_state)
+        # print("{}, {}".format(state, state_index))
+        # print("{}, {}".format(next_state, next_state_index))
+        # print("")
         next_max = np.max(Q[next_state_index])
         old_value = Q[state_index, action]
         new_value = old_value + alpha * (reward + gamma * next_max - old_value)
         Q[state_index, action] = new_value
         total_reward += reward
         state = next_state
+        state_index = next_state_index
         t = t + 1
         total_timesteps = total_timesteps + 1
-    if episode % 100 == 0:
+    if episode % 1000 == 0:
         print("Episode finished after {} timesteps".format(t+1))
         print("Average timesteps {}".format(total_timesteps / (episode+1)))
 env.close()
