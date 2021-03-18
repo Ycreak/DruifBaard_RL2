22d3dd427324a4dbd2c4f7bdb52b9d691ec5a490
ppbors <ppbors@hotmail.com>
2021-03-18 12:17:42 +0100

    Changed step size and memory usage

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ import random
 # These are the limits we use for the observation values
 LIM_CART_POSITION = 2.4 # real: 2.4
 LIM_CART_VELOCITY = 1.0 # real: inf
-LIM_POLE_ANGLE = 12.0   # real: 48
+LIM_POLE_ANGLE = 12     # real: 48
 LIM_POLE_ROTATION = 25  # real: inf
 
 # These are the step sizes we use
 STEP_CART_POSITION = 0.01
 STEP_CART_VELOCITY = 0.01
-STEP_POLE_ANGLE = 0.1
-STEP_POLE_ROTATION = 0.1
+STEP_POLE_ANGLE = 1
+STEP_POLE_ROTATION = 1
 
 NUMSTEPS_CART_POSITION = int((LIM_CART_POSITION * 2) / STEP_CART_POSITION)
 NUMSTEPS_CART_VELOCITY = int((LIM_CART_VELOCITY * 2) / STEP_CART_VELOCITY)
@@ -50,8 +50,8 @@ def discretize(observation) -> int: # Maybe reject values outside selected space
     # calculate orders first
     cart_position = int((round(cart_position, 2) + LIM_CART_POSITION) / STEP_CART_POSITION)
     cart_velocity = int((round(cart_velocity, 2) + LIM_CART_VELOCITY) / STEP_CART_VELOCITY)
-    pole_angle = int((round(pole_angle, 1) + LIM_POLE_ANGLE) / STEP_POLE_ANGLE)
-    pole_rotation = int((round(pole_rotation, 1) + LIM_POLE_ROTATION) / STEP_POLE_ROTATION)
+    pole_angle = int((round(pole_angle, 0) + LIM_POLE_ANGLE) / STEP_POLE_ANGLE)
+    pole_rotation = int((round(pole_rotation, 0) + LIM_POLE_ROTATION) / STEP_POLE_ROTATION)
 
     index = cart_position
     index = index + NUMSTEPS_CART_POSITION * cart_velocity
@@ -64,6 +64,7 @@ def Q_learning():
     Q = np.zeros([STATE_SIZE, ACTION_SIZE])
 
     print("Total matrix size: {}x{}".format(STATE_SIZE, ACTION_SIZE))
+    print("Estimated GB used: {}GB".format(Q.nbytes/1000000000))
     print_per_n = 1000
 
     env = gym.make('CartPole-v0')
